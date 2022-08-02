e9eb0779655500f7ca6245790d2cb1126b48f3a9
rdanf1 <reylet.daniel@gmail.com>
2022-08-02 17:12:40 +0200

    Pertes en Bourse, in progress...

--- Pertes_en_Bourse/Pertes_en_Bourse.py
+++ Pertes_en_Bourse/Pertes_en_Bourse.py
@@ -4,12 +4,41 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+no_prev = True
+a_zero = False
+input_list = []
+prev_v = 0
+lost = 0
+
 n = int(input())
 for i in input().split():
     v = int(i)
+    print("v : ", v, file=sys.stderr, flush=True)
+    input_list.append(v)
+    
+    #""" TODO without a list
+    if no_prev :
+        no_prev = False
+        prev_v = v
+    elif prev_v >= v :
+        lost += prev_v - v
+        
+    
+    """ A jump (false) start with list
 
+max_value = max(input_list)
+max_index = input_list.index(max_value)
+
+min_value = min(input_list)
+min_index = input_list.index(min_value)
+
+if min_index > max_index :
+    lost = max_value - min_value
+
+    """
+    
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print("answer")
+print(lost)
 
