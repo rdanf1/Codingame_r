d7b1170176d695802b343d61c809e9601107eba3
rdanf1 <reylet.daniel@gmail.com>
2022-08-09 11:30:32 +0200

    Shadows_Of_The_Knight (EP1) : Initialised

---
+++ Shadows_Of_The_Knight_EP1.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+# w: width of the building.
+# h: height of the building.
+w, h = [int(i) for i in input().split()]
+n = int(input())  # maximum number of turns before game over.
+x0, y0 = [int(i) for i in input().split()]
+
+# game loop
+while True:
+    bomb_dir = input()  # the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+    # the location of the next window Batman should jump to.
+    print("0 0")
+
